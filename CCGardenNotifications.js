0a1e5bff9739029b4193099a63f004b623e08d9d
Domilego4 <repi_ro@hotmail.com>
2019-11-18 11:43:02 -0400

    Update CCGardenNotifications.js
    added comments

--- CCGardenNotifications.js
+++ CCGardenNotifications.js
@@ -1,5 +1,19 @@
-// Code poorly written by u/Domilego4 (@Domilego4 on Twitter) while he was bored in class
+/*
+Code poorly written by u/Domilego4 (@Domilego4 on Twitter) while he was bored one time in class.
 
+A mod that annoyingly plays the Minecraft "Pling.ogg" sound effect 10 times
+whenever that one plant reaches the stage you were waiting for.
+
+How to use:
+- Open the Console
+- Type "ccX = [0-5]" to select the x coordinate of the garden tile to check
+- Type "ccY = [0-5]" to select the y coordinate of the garden tile to check
+	- I haven't tested these with smaller gardens, so it might break, but I don't think it should.
+- Type "ccStage = [0-4]" to select which stage you want a notification for.
+	- 0 is None (no notifications)
+	- 1, 2, 3 and 4 are Bud, Sprout, Bloom and Mature, respectively
+	- You can also replace the number with "ccStages.<stage name>"
+*/
 
 const ccStages =
 {
@@ -17,12 +31,11 @@ var ccChecked = false;
 
 function gardenCheck()
 {
-		PlaySound("https://gamepedia.cursecdn.com/minecraft_gamepedia/6/65/Pling.ogg",1,0);
+	PlaySound("https://gamepedia.cursecdn.com/minecraft_gamepedia/6/65/Pling.ogg",1,0);
 }
 
 function gardenNotification(x, y, stage)
 {
-	
 	if(document.getElementById("gardenTileIcon-"+x+"-"+y).style.backgroundPositionX === "-"+stage*48+"px")
 	{
 		ccStage = ccStages.none;
